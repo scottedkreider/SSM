0958d6a923c7e6b343e2e65e3ddfd171b6892724
scottedkreider <scottedkreider@gmail.com>
2022-09-18 19:01:40 -0400

    adding testing

--- test/Authorization.js
+++ test/Authorization.js
@@ -1,23 +1,16 @@
+const fetch = require("node-fetch");
+
+// import fetch from "node-fetch";
+
 QUnit.module('(Auth) Successfully create new account');
 
-// QUnit.test('(1) Submit unique username and password', assert => {
-//     const credentials = {
-//         username: "user",
-//         email: "user@scotttest.com",
-//         password: "TheSecondBestPassword",
-//         roles: ["user"]
-//     }
-//     const register_req = new XMLHttpRequest();
+QUnit.test('(1) Access public content', assert => {
+    const done = assert.async();
 
-//     register_req.open("POST","/api/auth/signup",true)
-//     register_req.send(JSON.stringify(credentials));
-
-//     assert.equal(register_req.response.status,200);
-// })
-
-QUnit.test('(2) Access public content', assert => {
-    var register_req = fetch("/api/test/all", {
-        method: "GET"
-    }).status
-    assert.equal(register_req,200);
+    fetch('http://localhost:5000/api/test/all')
+    .then((response) => {
+        assert.equal(response.status, 200);
+        done();
+        }
+    );
 })
