af2bff010a4511998fb0045de02188b11ff83b60
scottedkreider <scottedkreider@gmail.com>
2022-09-19 00:06:12 -0400

    roles removed

--- test/Authorization.js
+++ test/Authorization.js
@@ -14,3 +14,53 @@ QUnit.test('(1) Access public content', assert => {
         }
     );
 })
+
+
+QUnit.test('(2) Attempt to register a username that is already created', assert => {
+    const done = assert.async();
+
+    const data = {
+        "username": "user1",
+        "email": "userabcd@scotttest.com",
+        "password": "TheSecondBestPassword"
+    }
+
+    fetch('http://localhost:5000/api/auth/signup',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        assert.equal(response.status, 401);
+        done();
+        }
+    );
+})
+
+
+
+
+QUnit.test('(2) Attempt to register an email that is already created', assert => {
+    const done = assert.async();
+
+    const data = {
+        "username": "user2",
+        "email": "user1@scotttest.com",
+        "password": "TheSecondBestPassword"
+    }
+
+    fetch('http://localhost:5000/api/auth/signup',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => {
+        assert.equal(response.status, 402);
+        done();
+        }
+    );
+})
