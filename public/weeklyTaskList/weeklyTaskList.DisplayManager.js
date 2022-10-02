e1d4060efbc88a400e357e9fbff51acbca90c347
scottedkreider <scottedkreider@gmail.com>
2022-10-02 19:24:54 -0400

    start new weekly task list successfully saving in mongo

--- public/weeklyTaskList/weeklyTaskList.DisplayManager.js
+++ public/weeklyTaskList/weeklyTaskList.DisplayManager.js
@@ -2,20 +2,34 @@
 // Get div element to hold the Weekly Task List
 const weeklyTaskList_div = document.getElementById("weeklyTaskListDiv");
 
+// If there is not a currently existing weekly task list in
+//      local storage then show the "Create new weekly task
+//      list" button
+// Else display the weekly task list that is in local storage
 
-// Build the button
-// startNewWeeklyTaskListButton_div: Start New Weekly Task List Button
-const startNewWeeklyTaskListButton_div = document.createElement("div");
-const startNewWeeklyTaskList_button = document.createElement("button");
-startNewWeeklyTaskList_button.textContent = "Start New Weekly Task List"
-startNewWeeklyTaskList_button.id = "startNewWeeklyTaskList_button"
-startNewWeeklyTaskListButton_div.append(startNewWeeklyTaskList_button);
+const currentWeeklyTaskList = localStorage.getItem("weeklyTaskList_model")
 
-// Display the button on the interface
-weeklyTaskList_div.append(startNewWeeklyTaskList_button)
+!currentWeeklyTaskList
+    ? displayStartNewWeeklyTaskListButton()
+    : displayWeeklyTaskList();
 
-// Listen for the button
-startNewWeeklyTaskList_button.addEventListener('click',() => startNewWeeklyTaskList());
+
+function displayStartNewWeeklyTaskListButton() {
+    // Build the button
+    // startNewWeeklyTaskListButton_div: Start New Weekly Task List Button
+    const startNewWeeklyTaskListButton_div = document.createElement("div");
+    const startNewWeeklyTaskList_button = document.createElement("button");
+    startNewWeeklyTaskList_button.textContent = "Start New Weekly Task List"
+    startNewWeeklyTaskList_button.id = "startNewWeeklyTaskList_button"
+    startNewWeeklyTaskListButton_div.append(startNewWeeklyTaskList_button);
+
+    // Display the button on the interface
+    weeklyTaskList_div.append(startNewWeeklyTaskList_button)
+
+    startNewWeeklyTaskList_button.addEventListener('click',
+        startNewWeeklyTaskList());
+}
+
 
 // Upon hearing the button - start a new Weekly Task List
 function startNewWeeklyTaskList() {
@@ -25,7 +39,7 @@ function startNewWeeklyTaskList() {
     };
 
     // Save the new Weekly Task List to local storage
-    localStorage.setItem("weeklyTaskList_model",JSON.stringify(weeklyTaskList_model));
+    localStorage.setItem("weeklyTaskList_model", JSON.stringify(weeklyTaskList_model));
 
     // Clear the screen
     weeklyTaskList_div.replaceChildren();
@@ -36,9 +50,24 @@ function startNewWeeklyTaskList() {
         document.createTextNode(localStorage.getItem("weeklyTaskList_model"));
     newWeeklyTaskList_div.append(newWeeklyTaskListTitle_text);
     weeklyTaskList_div.append(newWeeklyTaskList_div);
+
+    // Listen for navigating away from the page and send weeklyTaskList_model to the database
+        console.log("left the page")
+        fetch('http://localhost:5000/api/weeklyTaskList', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: `${localStorage.getItem("weeklyTaskList_model")}`
+        })
+            .then((response) => {
+                console.log(response.status);
+            }
+            );
+
+
 }
 
-// Listen for navigating away from the page and send weeklyTaskList_model to the database
-window.addEventListener("onunload",() => console.log("left the page"));
-
-
+function displayWeeklyTaskList() {
+    console.log("lmao")
+}
