6a10da9b993f55110f80fb146d900249d8a5f21e
scottedkreider <scottedkreider@gmail.com>
2022-10-06 21:59:20 -0400

    added mmc

--- public/multiMonthCalendar/multiMonthCalendar.DisplayManager.js
+++ public/multiMonthCalendar/multiMonthCalendar.DisplayManager.js
@@ -1,100 +1,24 @@
 "use strict";
 
-// import
-//     { submitDatesListener }
-//     from "./multiMonthCalendar.EventListeners.js";
-// import
-//     {   mmcDateEntryForm    }
-//     from "./multiMonthCalendar.divSnippets.js";
+import MultiMonthCalendarDisplay from "./multiMonthCalendar.Interface.js";
+// import MultiMonthCalendarManager from "./multiMonthCalendar.Model";
 
 export default function displayMultiMonthCalendar(){
-    const indexDiv = document.getElementById("multiMonthCalendarDiv");
-    const indexH1 = document.createElement("h1");
-    indexH1.innerText = "MultiMonth Calendar";
+    const multiMonthCalendarDiv = document.getElementById("multiMonthCalendarDiv");
+    multiMonthCalendarDiv.innerHTML = "<h1>MultiMonth Calendar</h1>";
 
-    // if(!localStorage.getItem("_multiMonthCalendar")){
-    function buildMultiMonthCalendarDateEntryDisplay(){
-        multiMonthCalendarDiv.appendChild(mmcDateEntryForm());
-        submitDatesListener();
+    var multiMonthCalendarDisplay = new MultiMonthCalendarDisplay();
+
+    if(!localStorage.getItem("_multiMonthCalendar")){
+        multiMonthCalendarDisplay.buildMultiMonthCalendarDateEntryDisplay();
+    } else {
+
+        var _mgr = JSON.parse(localStorage.getItem("_multiMonthCalendar"));
+        console.log(_mgr);
+
+        multiMonthCalendarDisplay.insertMultiMonthCalendarToDisplay(JSON.parse(localStorage.getItem("_multiMonthCalendar")));
+        multiMonthCalendarDisplay.buildMultiMonthCalendarDisplay();
     }
-    buildMultiMonthCalendarDateEntryDisplay();
-}
-
-function mmcDateEntryForm(){
-    var today = new Date();
-
-    var _mmcDateEntryDiv = document.createElement('div');
-    _mmcDateEntryDiv.setAttribute("class","mmcDateEntryDiv");
-
-    var _mmcDateEntryForm = document.createElement('form');
-    _mmcDateEntryForm.setAttribute("class","mmcDateEntryForm");
-
-    var startDateDiv = document.createElement("div");
-    startDateDiv.innerHTML = "Enter MultiMonth Period Start Date";
-    var startDateInput = document.createElement("input");
-    startDateInput.setAttribute("type","date");
-    startDateInput.setAttribute("id","mmcStartDate");
-    // Update to have dynamic date around the current date
-    startDateInput.setAttribute("value","2022-05-01");
-    startDateDiv.appendChild(startDateInput);
-
-    var endDateDiv = document.createElement("div");
-    endDateDiv.innerHTML = "Enter MultiMonth Period End Date";
-    var endDateInput = document.createElement("input");
-    endDateInput.setAttribute("type","date");
-    endDateInput.setAttribute("id","mmcEndDate");
-    // Update to have dynamic date around the current date
-    endDateInput.setAttribute("value","2022-08-19");
-    endDateDiv.appendChild(endDateInput);
-
-    var mmcTitleDiv = document.createElement("div");
-    mmcTitleDiv.innerHTML = "Enter MultiMonth Period Title";
-    var mmcTitleInput = document.createElement("input");
-    mmcTitleInput.setAttribute("id","mmcTitle");
-    mmcTitleInput.setAttribute("placeholder","Title");
-    mmcTitleDiv.appendChild(mmcTitleInput);
-
-    var submitDatesButton = document.createElement("button");
-    submitDatesButton.setAttribute("type","button");
-    submitDatesButton.setAttribute("id","mmcDateSubmitButton");
-    submitDatesButton.innerHTML = "Submit";
-
-    _mmcDateEntryForm.appendChild(startDateDiv);
-    _mmcDateEntryForm.appendChild(endDateDiv);
-    _mmcDateEntryForm.appendChild(mmcTitleDiv);
-    _mmcDateEntryForm.appendChild(submitDatesButton);
-
-    return _mmcDateEntryForm;
-}
-
-
-function submitDatesListener(){
-    const _submitButton = document.getElementById("mmcDateSubmitButton");
-    // console.log(_submitButton);
-
-    let _mmc_mgr;
-
-    _submitButton.addEventListener("click",() => {
-        // console.log("weSubmitting");
-        const _startDate = document.getElementById("mmcStartDate");
-        const _endDate = document.getElementById("mmcEndDate");
-        const _mmcTitle = document.getElementById("mmcTitle");
-
-        _mmc_mgr = {
-            startdate: _startDate.value,
-            enddate: _endDate.value,
-            _mmcTitle: _mmcTitle.value
-        }
-
-        localStorage.setItem("_multiMonthCalendar",JSON.stringify(_mmc_mgr));
-        // localStorage.setItem("_multiMonthCalendar",JSON.stringify(_mmc_mgr._mmc));
-        // localStorage.setItem("_mgr",JSON.stringify(_mmc_mgr));
-        // localStorage.setItem("_lastSent",JSON.stringify(true));
-
-        // sendMultiMonthCalendarToDb(_mmc_mgr);
-
-        // displayMultiMonthCalendar();
-    })    
 }
 
 displayMultiMonthCalendar();
--- public/multiMonthCalendar/multiMonthCalendar.Interface.js
+++ public/multiMonthCalendar/multiMonthCalendar.Interface.js
@@ -1,69 +1,63 @@
 "use strict";
 
-// import {
-//     mmcDateEntryForm, mmcCalendarDiv, deleteMMCDiv, checkAllDaysDiv,
-//     editDailyInfoDiv
-// } from "./multiMonthCalendar.divSnippets.js";
-// import {
-//     submitDatesListener, checkOffDaysListener,
-//     deleteMMCListener, checkAllDaysListener,
-//     editDailyInfoListener
-// } from "./multiMonthCalendar.EventListeners.js";
-// import MultiMonthCalendarManager from "./multiMonthCalendar.Model.js";
+import {
+    mmcDateEntryForm, mmcCalendarDiv, deleteMMCDiv, checkAllDaysDiv,
+    editDailyInfoDiv
+} from "./multiMonthCalendar.divSnippets.js";
+import {
+    submitDatesListener, checkOffDaysListener,
+    deleteMMCListener, checkAllDaysListener,
+    editDailyInfoListener
+} from "./multiMonthCalendar.EventListeners.js";
+import MultiMonthCalendarManager from "./multiMonthCalendar.Model.js";
 
-// export default class MultiMonthCalendarDisplay {
-        // var _mmc = null;
-        const multiMonthCalendarDiv = document.getElementById("multiMonthCalendarDiv");
+export default class MultiMonthCalendarDisplay {
+    constructor() {
+        // Div holding the entire MultiMonthCalendar
 
+        this._mmc = null;
+        this.multiMonthCalendarDiv = document.getElementById("multiMonthCalendarDiv");
+    }
 
     // Take in a mmc to display on the screen
     // Contract: mmc is not null
     //           mmc is of type MultiMonthCalendarManager
-    export default function insertMultiMonthCalendarToDisplay(mmc) {
-        _mmc = mmc;
+    insertMultiMonthCalendarToDisplay(mmc) {
+        this._mmc = mmc;
     }
 
-    export function buildMultiMonthCalendarHeader() {
+    buildMultiMonthCalendarHeader() {
         this.multiMonthCalendarDiv.setAttribute("class", "multiMonthCalendarDisplay");
-
-        const mmcH1 = document.createElement("h1");
-        mmcH1.innerText = "MultiMonthCalendar"
-
-        const mmcH2 = document.createElement("h1");
-        mmcH2.innerText = `Title: ${_mmc.Title}`;
-
-        const mmcp = document.createElement("p");
-        mmcp.innerText = `Dates: ${_mmc.StartDate} to ${this._mmc.EndDate}`;
-        
-        multiMonthCalendarDiv.appendChild(mmcH1);
-        multiMonthCalendarDiv.appendChild(mmcH2);
-        multiMonthCalendarDiv.appendChild(mmcp);
-
-        multiMonthCalendarDiv.appendChild(deleteMMCDiv());
+        this.multiMonthCalendarDiv.innerHTML =
+            `<h1>MultiMonthCalendar</h1>
+            <h2>Title: ${this._mmc.Title}</h2>
+            <p>Dates: ${this._mmc.StartDate} to ${this._mmc.EndDate}</p>
+            `;
+        this.multiMonthCalendarDiv.appendChild(deleteMMCDiv());
         deleteMMCListener();
 
-        multiMonthCalendarDiv.appendChild(checkAllDaysDiv());
-        checkAllDaysListener(_mmc);
+        this.multiMonthCalendarDiv.appendChild(checkAllDaysDiv());
+        checkAllDaysListener(this._mmc);
 
-        multiMonthCalendarDiv.appendChild(editDailyInfoDiv());
-        editDailyInfoListener(_mmc);
+        this.multiMonthCalendarDiv.appendChild(editDailyInfoDiv());
+        editDailyInfoListener(this._mmc);
     }
 
     // Populate the mmcDiv with the calendar
-    export function buildMultiMonthCalendarDisplay() {
-        buildMultiMonthCalendarHeader();
-        mmcCalendarDiv(multiMonthCalendarDiv, _mmc);
-        checkOffDaysListener(_mmc);
+    buildMultiMonthCalendarDisplay() {
+        this.buildMultiMonthCalendarHeader();
+        mmcCalendarDiv(this.multiMonthCalendarDiv, this._mmc);
+        checkOffDaysListener(this._mmc);
     }
 
-    export function addNewMultiMonthCalendar(startDate, endDate, title){
+    addNewMultiMonthCalendar(startDate, endDate, title){
         this._mmc = new MultiMonthCalendarManager(startDate, endDate, title)._mmc;
 
         return this._mmc;
     }
 
-    // export default function buildMultiMonthCalendarDateEntryDisplay(){
-    //     multiMonthCalendarDiv.appendChild(mmcDateEntryForm());
-    //     submitDatesListener();
-    // }
-// }
+    buildMultiMonthCalendarDateEntryDisplay(){
+        this.multiMonthCalendarDiv.appendChild(mmcDateEntryForm());
+        submitDatesListener();
+    }
+}
