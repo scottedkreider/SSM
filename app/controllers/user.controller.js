af3026ef20bf17b56c46a60dadf3eb63655eb116
scottedkreider <scottedkreider@gmail.com>
2022-10-14 16:31:42 -0400

    Update user.controller.js

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,14 +1,6 @@
 const db = require("../models");
 const User = db.user;
 
-exports.allAccess = (req, res) => {
-    res.status(200).send("Public Content.");
-  };
-  
-  exports.userBoard = (req, res) => {
-    res.status(200).send("User Content.");
-  };
-
   exports.deleteAccount = (req, res) => {
       User.deleteOne({
         username: req.body.username
