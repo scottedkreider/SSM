1ec3647a919ae21a6b2f044ae470a1b2a2fb5aa1
scottedkreider <scottedkreider@gmail.com>
2022-10-20 21:23:17 -0400

    Update weeklyCalendar.displayManager.js

--- public/weeklyCalendar/weeklyCalendar.displayManager.js
+++ public/weeklyCalendar/weeklyCalendar.displayManager.js
@@ -1,10 +1,3 @@
 const weeklyCalendarDiv = document.getElementById("weeklyCalendarDiv");
 
-let ec = new EventCalendar(document.getElementById('weeklyCalendarDiv'), {
-    view: 'timeGridWeek',
-    events: [
-        // your list of events
-    ]
-});
-
-// weeklyCalendarDiv.innerText = "Weekly Calendar"
+weeklyCalendarDiv.innerText = "Weekly Calendar"
